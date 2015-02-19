107d48b80530bb5ef95ed11769080b1f3176a542
Ashley <amburke42@gmail.com>
2015-02-20 08:27:40 +1000

    Unsubscribe from email lists

---
+++ content.js
@@ -0,0 +1,53 @@
+/* Inform the backgrund page that 
+ * this tab should have a page-action */
+chrome.runtime.sendMessage({
+    from:    'content',
+    subject: 'showPageAction'
+});
+
+
+/* Listen for message from the popup */
+chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+    /* Validate the message's structure */
+    if ((msg.from === 'popup') && (msg.subject === 'status')) {
+        var x = document.links;
+		var href;
+		var linkFound = false;
+		var linkText= "";
+
+
+		if (window.find("unsubscrib")){
+
+
+			/* Loop through all links */
+			for (i = 0; i < x.length; i++) {
+				/*Find all unsubscribe links*/
+				linkText = x[i].text;
+				if (linkText.indexOf("Unsubscribe") > -1 ||	linkText.indexOf("unsubscribe") > -1 ){
+			    	linkFound = true;
+			    	href = x[i].href;
+			    	break;
+			    }
+
+			    else if (linkText.indexOf("click here") > -1 || linkText.indexOf("here") > -1 || linkText.indexOf("Here") > -1 ){
+			    	linkFound = true;
+			    	href = x[i].href;
+			    }
+			}
+
+		}
+
+		
+		var info = {
+			isLink: linkFound,
+			url: href
+		};
+
+		/* Directly respond to the sender (popup), 
+         * through the specified callback */
+        response(info);
+    }
+
+});
+
+
---
+++ popup.js
@@ -0,0 +1,61 @@
+function setStatus(status) {
+    document.getElementById('status').textContent   = status;
+}
+
+function setInfo(info) {
+    var isLink = info.isLink;
+    url = info.url;
+    if (isLink){
+        setStatus("Are you sure you want to unsubscribe?");
+        document.getElementById('yesButton').style.display = 'inline';
+        document.getElementById('noButton').style.display = 'inline';
+    
+    }
+    else
+        setStatus("No unsubscribe link found.");
+}
+
+
+function quit(){
+    window.close();
+}
+
+function unsubscribe(){
+    setStatus("Unsubscribing");
+    document.getElementById('yesButton').style.display = 'none';
+    document.getElementById('noButton').style.display = 'none';
+    chrome.tabs.create({ url: url });
+
+}
+
+function findUnsubscribe(){
+    document.getElementById('yesButton').style.display = 'none';
+    document.getElementById('noButton').style.display = 'none';
+
+    setStatus("Unsubscribing now...");
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+
+        /* Send a request for the unsubscribe info... */
+        chrome.tabs.sendMessage(
+                tabs[0].id,
+                {from: 'popup', subject: 'status'},
+                /* Specifya callback to be called 
+                 *    from the receiving end (content script) */
+                setInfo);
+    });
+
+
+}
+
+var url = "";
+
+/*First - find a link to unsubscribe from*/
+setStatus("Looking for unsubscribe link.");
+/*Wait for DOMContent to load*/
+window.addEventListener('DOMContentLoaded', findUnsubscribe);
+
+document.getElementById('yesButton').addEventListener('click', unsubscribe);
+document.getElementById('noButton').addEventListener('click', quit);
